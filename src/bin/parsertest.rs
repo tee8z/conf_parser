42a90135339335bd7edb34427a3933e8979ba9a7
teebz <5day4cast@protonmail.com>
2023-06-30 20:31:08 -0400

    stubbing out testing for this lib

--- src/bin/parsertest.rs
+++ src/bin/parsertest.rs
@@ -1,6 +1,8 @@
 use std::fs;
 
 fn main() {
-    let contents = fs::read_to_string("lnd-sample.conf").expect("file read error");
+    let contents = fs::read_to_string("./test_files/sample_lnd.conf").expect("file read error");
+    let contents = fs::read_to_string("./test_files/sample_bitcoind.conf").expect("file read error");
+
     //let lnd_conf 
 }
