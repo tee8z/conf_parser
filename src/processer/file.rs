c6046c37d70ed3193de22dfb5603387a4520529c
teebz <5day4cast@protonmail.com>
2023-07-10 00:59:26 -0400

    adding new access function to section

--- src/processer/file.rs
+++ src/processer/file.rs
@@ -1,5 +1,7 @@
 use std::collections::HashMap;
 
+//TODO: add methods to make it easy to update these values
+
 #[derive(Debug)]
 pub struct FileConf {
     pub sections: HashMap<String, Section>
@@ -21,5 +23,11 @@ pub struct Section {
 impl Section {
     pub fn new() -> Self {
         Section { properties: HashMap::new() }
+    }
+    pub fn get_property(self, name: &str) -> String {
+        match self.properties.get(name){
+            Some(item) => item.to_string(),
+            None => "".to_string()
+        }
     }
 }
